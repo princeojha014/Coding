83668bdc7581104e0543674a795febd535396825
Prince Ojha <princeojha014@gmail.com>
2024-07-29 22:01:19 +0530

    Code

--- Java/Kunal Kushwaha/Practice/4. Functions/SumFun.java
+++ Java/Kunal Kushwaha/Practice/4. Functions/SumFun.java
@@ -4,10 +4,21 @@ public class SumFun
 {
     public static void main(String[] args) 
     {  
-        sum();
+        int ans = sum();
     }
     
-    static void sum()
+    static int sum()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter 1st Number : ");
+        int n1 = input.nextInt();
+        System.out.print("Enter 2nd Number : ");
+        int n2 = input.nextInt();
+        int sum = n1+n2;
+        return sum;
+    }
+    
+    /*static void sum()
         {
             Scanner input = new Scanner(System.in);
             System.out.print("Enter 1st Number : ");
@@ -16,5 +27,5 @@ public class SumFun
             int n2 = input.nextInt();
             int sum = n1+n2;
             System.out.println("Sum of digits are " +sum);
-        }
-}
+        }*/
+}     
